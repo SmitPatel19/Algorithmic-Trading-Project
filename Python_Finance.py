bbd5e56aa9113df972096e663d0636105e791a1b
Spatel4 <57647348+SmitPatel19@users.noreply.github.com>
2020-03-26 22:24:51 -0400

    Update Python_Finance.py

--- Python_Finance.py
+++ Python_Finance.py
@@ -10,7 +10,7 @@ import sklearn
 import datetime as dt
 import matplotlib.pyplot as plt
 from matplotlib import style 
-from mplfinance import candlestick_ohlc
+from mpl_finance import candlestick_ohlc #need to confirm versions/compatability
 import pandas as pd
 import pandas_datareader.data as web
 
@@ -47,20 +47,24 @@ df_ohlc = df['Adj Close'].resample('10D').ohlc()
 df_volume = df['Volume'].resample('10D').sum()
 
 df_ohlc.reset_index(inplace=True)
+df_ohlc['Date'] = df_ohlc['Date'].map(mdates.date2num)
 print(df_ohlc.head())
-
-
-
-
 
 ax1 = plt.subplot2grid((6,1), (0,0), rowspan=5, colspan=1)
 ax2 = plt.subplot2grid((6,1), (5,0), rowspan=1, colspan=1, sharex=ax1)
+ax1.xaxis_date()
+
+candlestick_ohlc(ax1,df_ohlc.values,width=2, colorup= 'g')
+ax2.fill_between(df_volume.index.map(mdates.date2num), df_volume.values, 0)
 
 # ax1.plot(df.index, df['Adj Close'])
 # ax1.plot(df.index, df['100ma'])
 # ax2.bar(df.index, df['Volume'])
 
-#plt.show()
+plt.show()
+
+#--------------------3/XX/2020---------------------------------------
+
 
 
 
