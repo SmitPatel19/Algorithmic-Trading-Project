bdf6a88ac40441871bff435308bdf32246f10ff8
Spatel4 <57647348+SmitPatel19@users.noreply.github.com>
2020-03-28 01:09:10 -0400

    Combined multiple df's into one

--- Python_Finance_2.py
+++ Python_Finance_2.py
@@ -31,7 +31,7 @@ def save_sp500_tickers():
 
     return tickers
 
-save_sp500_tickers()
+#save_sp500_tickers()
 
 #----------------------3/28/2020--------------------------------
 
@@ -58,6 +58,36 @@ def get_data_from_yahoo(reload_sp500=False):
 
 
 #get_data_from_yahoo()
+
+def compile_data():
+    with open("sp500tickers.pickle","rb") as f:
+        tickers = pickle.load(f)
+
+    main_df = pd.DataFrame()
+
+    for count,ticker in enumerate(tickers):
+        df = pd.read_csv('stock_dfs/{}.csv'.format(ticker))
+        df.set_index('Date',inplace=True)
+
+        df.rename(columns = {'Adj Close' : ticker}, inplace=True)
+        df.drop(['Open','High','Low','Close','Volume'], 1, inplace=True)
+
+        if main_df.empty:
+            main_df = df
+        else:
+            main_df = main_df.join(df, how='outer')
+        
+        if count % 10 == 0:
+            print(count)
+
+    print(main_df.head())
+    main_df.to_csv('sp500_joined_closes.csv')
+
+compile_data()
+
+#-----------------------3/XX/2020---------------------------------
+
+
 
 
 
