e1565c1d9e0077262ac51d2fa67883f9de118d61
Spatel4 <57647348+SmitPatel19@users.noreply.github.com>
2020-03-29 00:12:19 -0400

    Update Python_Finance_3.py

--- Python_Finance_3.py
+++ Python_Finance_3.py
@@ -1,4 +1,5 @@
 #------------------3/28/2020----------------------------------
+from collections import Counter
 import numpy as np
 import pandas as pd
 import pickle 
@@ -15,5 +16,49 @@ def process_data_for_labels(ticker):
     df.fillna(0, inplace=True)
     return tickers,df
 
+#process_data_for_labels('XOM')
 
-process_data_for_labels('XOM')
+def buy_sell_hold(*args):
+    cols = [c for c in args]
+    requirement = 0.02
+    for col in cols:
+        if col > requirement:
+            return 1 #BUY
+        if col < -requirement:
+            return -1 #SELL
+    return 0 #HOLD
+
+def extract_featuresets(ticker):
+    tickers, df = process_data_for_labels(ticker)
+
+    df['{}_target'.format(ticker)] = list(map( buy_sell_hold,
+                                               df['{}_1d'.format(ticker)],
+                                               df['{}_2d'.format(ticker)],
+                                               df['{}_3d'.format(ticker)],
+                                               df['{}_4d'.format(ticker)],
+                                               df['{}_5d'.format(ticker)],
+                                               df['{}_6d'.format(ticker)],
+                                               df['{}_7d'.format(ticker)],
+                                               ))
+    
+    vals = df['{}_target'.format(ticker)].values.tolist()
+    str_vals = [str(i) for i in vals]
+    print('Data spread:', Counter(str_vals))
+    
+    df.fillna(0, inplace=True)
+    df = df.replace([np.inf, -np.inf], np.nan)
+    df.dropna(inplace=True)
+
+    df_vals = df[[ticker for ticker in tickers]].pct_change()
+    df_vals = df_vals.replace([np.inf, -np.inf], 0)
+    df_vals.fillna(0, inplace=True)
+
+    X = df_vals.values
+    Y = df['{}_target'.format(ticker)].values
+
+    return X, Y, df
+
+extract_featuresets('XOM')
+
+#----------------------3/29/2020-------------------------------------------------
+
